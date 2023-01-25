4ca63094644d4fa4ce82ef44576ed6ce7d837202
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-01-24 22:30:04 -0300

    Crinado a pasta dos componentes e criandoo o module.css para criar css dentro do escopo do componetne

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Post from './Post'
 import './App.css'
+import { Header } from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <div>
+   <Header/>
    <Post 
    author= "Reinaldo"
    content = "EBaaaaaaaaaa"
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import styles from './Header.module.css'
+
+export function Header() {
+    return(
+    <strong className={styles.header}>Ignite Feed</strong>
+    )
+
+}

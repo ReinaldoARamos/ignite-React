d0cf0d66734500ae44ecb523743c112bff9e8527
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-01-26 22:26:20 -0300

    adicionando estilo na imagem do perfil

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,18 +4,23 @@ export function Sidebar() {
   return (
     <div>
       <aside className={styles.sidebar}>
-        <img src="https://images.unsplash.com/photo-1501339847302-ac426a4a7cbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"></img>
-        <div>
-        <strong>Reinaldo Ramos</strong>
+        <img
+          className={styles.cover}
 
-        <span>Web Dev</span>
+          src="https://images.unsplash.com/photo-1501339847302-ac426a4a7cbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50">
 
-        <footer>
-          <a href="#">Editar Perfil</a>
-        </footer>
-      </div>
+          </img>
+          
+        <div className={styles.profile}>
+          <strong>Reinaldo Ramos</strong>
+
+          <span>Web Dev</span>
+
+          <footer>
+            <a href="#">Editar Perfil</a>
+          </footer>
+        </div>
       </aside>
-    
     </div>
   );
 }

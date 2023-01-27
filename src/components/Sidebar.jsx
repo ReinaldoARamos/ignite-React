2899931732f0c52c24778b0e09f29f003f9838ab
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-01-26 22:17:09 -0300

    adicionando estilo na sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,21 @@
-import styles from './Sidebar.module.css'
+import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
+  return (
+    <div>
+      <aside className={styles.sidebar}>
+        <img src="https://images.unsplash.com/photo-1501339847302-ac426a4a7cbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"></img>
+        <div>
+        <strong>Reinaldo Ramos</strong>
 
-    return(
-        <sidebar classname={styles.Sidebar} ><img src='https://images.unsplash.com/photo-1501339847302-ac426a4a7cbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50'></img> </sidebar>
-    )
+        <span>Web Dev</span>
+
+        <footer>
+          <a href="#">Editar Perfil</a>
+        </footer>
+      </div>
+      </aside>
+    
+    </div>
+  );
 }

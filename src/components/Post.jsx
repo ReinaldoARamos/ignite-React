2e2934451dc07fbe9e94a2b2ef21281ce3615490
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-02-05 19:38:16 -0300

    Colocando o Comentario como Componente

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment"
 import styles from "./Post.module.css"
 export function Post() {
     return (
@@ -34,6 +35,9 @@ export function Post() {
                 </footer>
                 
             </form>
+            <div className={styles.commentList}>
+                <Comment />
+            </div>
         </article>
     )
        

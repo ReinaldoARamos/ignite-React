214a6a38a98a5e3512cdaf1ca859abe6f4034707
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-02-05 19:13:09 -0300

    criando a estrutura HTMl da sessão de comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,26 @@
-import styles from "./Comment.module.css"
+import styles from "./Comment.module.css";
 
 export function Comment() {
-    return (
-        <div className={styles.teste}>
-            Teste
+  return (
+    <div className={styles.comment}>
+      <img src="https://i.pinimg.com/236x/cb/7e/87/cb7e8749021558f097169a16046af054.jpg" />
+        <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+       <header>
+       <div className={styles.authorAndTime}>
+            <strong>Reinaldo Aparecido Ramos</strong>
+            <time title="11 de maio as 08:31" dateTime="2022-05-11 08:13:00">
+              Publico há 1h
+            </time>
+          </div>
+       </header>
+       <p>Parabens buxa</p>
+       </div>  
+        <footer>
+            Aplaudir
+        </footer>
         </div>
-    )
-
+   
+    </div>
+  );
 }

682c25ba44fa03dbc64cbee536d50aad120b725d
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-02-06 21:52:07 -0300

    Criando o componente do avatar

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,8 @@
+export function avatar(){
+    return(
+        <div>
+             <img src="https://i.pinimg.com/236x/cb/7e/87/cb7e8749021558f097169a16046af054.jpg" />
+        </div>
+       
+    )
+}

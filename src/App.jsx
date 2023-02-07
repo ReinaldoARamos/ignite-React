6a431e2bf3644cc7fe9d11d1a73740be2327d6bc
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-02-07 17:49:01 -0300

    criando um array que vai conter os posts, posts esse sao objetos

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,26 @@ import {Comment} from './components/Comment'
 import styles from './App.module.css'
 import './global.css'
 
-
+/*
+author: {avatar_url: "", name: "" , role: ""}
+publishedAt: Date
+content: String
+*/ 
+const post = [
+  {
+    author:{
+      avatarUrl: 'https://github.com/ReinaldoARamos.png',
+      name: "Reinaldo Ramos",
+      role: "Web Developer"
+    },
+    content: [
+      {type: 'paragraph', content: 'Esse é o tal jogo que é melhor que o god of war 4'},
+      {type: 'paragraph', content: 'de usuário para ter uma ideia de como fazer um orçamento'},
+      {type: 'paragraph', content: 'Para limpar o nome da empresa para que eu possa fazer um novo anuncio'}
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00')
+  },
+]
 function App() {
   const [count, setCount] = useState(0)
 
